d4e171c1a305e267b6fa13ba5f85eb6d5a1480db
stop-amertime <101000447+stop-amertime@users.noreply.github.com>
2025-12-01 12:19:15 +0000

    tidied up code

--- day-1/script.py
+++ day-1/script.py
@@ -1,23 +1,21 @@
-## python script that loads an array of strings from a text file into a list
-def load_strings_from_file(file_path):
-    with open(file_path, 'r') as file:
-        strings_list = [line.strip() for line in file.readlines()]
-    return strings_list
-
-test_array = "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82".split(" ")
-
 if __name__ == "__main__":
-    file_path = 'puzzle-input.txt'  
-    strings = load_strings_from_file(file_path)
+
+    test_array = "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82".split(" ")
+    strings = []
+    with open('day-1/puzzle-input.txt', 'r') as file:
+        strings = [line.strip() for line in file.readlines()]
+
+    ## Start pointing at 50
     pos = 50
     zero_visits = 0
+
+    ## Manually increment/decrement and track visits to position 0
     for command in strings:
-        print(f"Processing command: {command}, direction: {command[0]}, value: {int(command[1:])}")
         for i in range(int(command[1:])):
             pos += 1 if command[0] == 'R' else -1
             pos = pos % 100
             if pos == 0: zero_visits += 1
+    
     print(f"Number of visits to position 0: {zero_visits}")
-
 
         
